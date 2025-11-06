d67144650fe3705e14a3a69e73f7ecbbacfb0883
Margaret O'Neill <margaret@oneillfish.com>
2025-11-06 17:03:46 -0500

    set up swagger

--- backend/src/main/java/com/interview/repair_order/repository/RepairOrderRepository.java
+++ backend/src/main/java/com/interview/repair_order/repository/RepairOrderRepository.java
@@ -7,18 +7,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import org.springframework.data.domain.Pageable;
-import java.util.List;
 
 public interface RepairOrderRepository extends JpaRepository<RepairOrder, String> {
 
-    @Query("Select ro from RepairOrder ro JOIN FETCH ro.repairOrderLines")
-    List<RepairOrder> findAllWithLines();
-
-    @Query(value = "Select ro from RepairOrder ro JOIN FETCH ro.repairOrderLines",
-            countQuery = "select count(ro) from RepairOrder ro")
-    Page<RepairOrder> findAllWithLinesPageable(Pageable pageable);
+//    @Query("Select ro from RepairOrder ro JOIN FETCH ro.repairOrderLines")
+//    List<RepairOrder> findAllWithLines();
+//
+//    @Query(value = "Select ro from RepairOrder ro JOIN FETCH ro.repairOrderLines",
+//            countQuery = "select count(ro) from RepairOrder ro")
+//    Page<RepairOrder> findAllWithLinesPageable2(Pageable pageable);
 
     @EntityGraph(attributePaths = "repairOrderLines")
     @Query("Select ro from RepairOrder ro")
-    Page<RepairOrder> findAllWithLinesPageable2(Pageable pageable);
+    Page<RepairOrder> findAllWithLinesPageable(Pageable pageable);
 }
--- backend/src/main/java/com/interview/repair_order/service/RepairOrderService.java
+++ backend/src/main/java/com/interview/repair_order/service/RepairOrderService.java
@@ -1,16 +1,11 @@
 package com.interview.repair_order.service;
 
 import com.interview.repair_order.api.model.RepairOrderResponse;
-import com.interview.repair_order.domain.RepairOrder;
 import com.interview.repair_order.repository.RepairOrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -18,15 +13,15 @@ public class RepairOrderService {
 
     private RepairOrderRepository repairOrderRepository;
 
-    public List<RepairOrderResponse> getAll() {
-
-        return repairOrderRepository.findAllWithLines().stream().map(RepairOrderResponse::new)
-                .collect(Collectors.toList());
-
-    }
+//    public List<RepairOrderResponse> getAll() {
+//
+//        return repairOrderRepository.findAllWithLines().stream().map(RepairOrderResponse::new)
+//                .collect(Collectors.toList());
+//
+//    }
 
     public Page<RepairOrderResponse> getAllPaginated(Pageable pageable) {
 
-        return repairOrderRepository.findAllWithLinesPageable2(pageable).map(RepairOrderResponse::new);
+        return repairOrderRepository.findAllWithLinesPageable(pageable).map(RepairOrderResponse::new);
     }
 }
