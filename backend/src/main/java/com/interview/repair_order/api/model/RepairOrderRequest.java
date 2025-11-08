9e4c2b93cf9d04b7f984169ed0b41e0a90569a2e
Margaret O'Neill <margaret@oneillfish.com>
2025-11-08 12:47:35 -0500

    adding schema defs for swagger

--- backend/src/main/java/com/interview/repair_order/api/model/RepairOrderRequest.java
+++ backend/src/main/java/com/interview/repair_order/api/model/RepairOrderRequest.java
@@ -1,6 +1,7 @@
 package com.interview.repair_order.api.model;
 
 import com.interview.repair_order.domain.Status;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,20 +16,27 @@ import javax.validation.constraints.Size;
 public class RepairOrderRequest {
 
     @NotNull
+    @Schema(description = "The UUID for the related shopId")
     @Pattern(regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$",
             message = "must be a valid UUID")
     public String shopId;
 
     @NotNull
     @Size(max = 50, message = "must be less than 50 characters")
+    @Schema(description = "A user visible repair order identifier")
     public String externalRO;
 
     @NotNull
+    @Schema(description = "The status of the repair order")
     public Status status;
 
+    @Schema(description = "The odometer reading upon intake")
     public Integer odometerIn;
 
+    @Schema(description = "The odometer reading at service end")
     public Integer odometerOut;
 
+    @Schema(description = "Any notes to describe the issue.")
+    @Size(max = 1000, message = "must be less than 1000 characters")
     public String notes;
 }
